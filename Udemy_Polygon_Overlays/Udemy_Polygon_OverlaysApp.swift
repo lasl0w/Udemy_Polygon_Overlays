ba89dadb72343438417e2103dacc8e2d7f44521d
Tom Montgomery <montgomery3681@gmail.com>
2023-12-30 22:46:24 -0700

    Initial Commit

---
+++ Udemy_Polygon_Overlays/Udemy_Polygon_OverlaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_Polygon_OverlaysApp.swift
+//  Udemy_Polygon_Overlays
+//
+//  Created by tom montgomery on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_Polygon_OverlaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
